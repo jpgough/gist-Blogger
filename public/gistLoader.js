b49c57a1f04b85bfb1ab6e6843bc6f5838e5b151
Lee Hanxue <hanxue.developer@gmail.com>
2014-05-04 02:37:46 +0800

    Update from moski's repo to hanxue's repo

--- public/gistLoader.js
+++ public/gistLoader.js
@@ -1,6 +1,6 @@
 (function() {
     
-    var gistBloggerPath = "https://raw.github.com/moski/gist-Blogger/master/public/gistBlogger.js";
+    var gistBloggerPath = "https://rawgithub.com/hanxue/gist-Blogger/develop/public/gistBlogger.js";
 
     // Initialize the load the main script.
     try {
